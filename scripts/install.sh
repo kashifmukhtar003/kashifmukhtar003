2b114289005ca4bdb2cd4b0d482d06e2e5778512
kashifmukhtar003 <kashif.mukhtar003@gmail.com>
2022-11-14 17:26:54 +0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,6 @@ npm i -g electron-packager
 npm i -g electron-installer-debian
 sudo apt-get -y install fakeroot
 npm i --legacy-peer-deps
-echo "SENTRY_DSN=${{ secrets.SENTRY_DSN }}" >> .env
 npm run build:linux
 npm run package:linux
 pwd
