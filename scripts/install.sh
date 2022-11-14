8d0393e999fd4bbc065cb49ce3228d2f82a4308a
kashifmukhtar003 <kashif.mukhtar003@gmail.com>
2022-11-14 17:21:52 +0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-sudo apt-get install fakeroot -y 
+npm install --global yarn
+npm i -g electron-packager
+npm i -g electron-installer-debian
+sudo apt-get -y install fakeroot
+npm i --legacy-peer-deps
+echo "SENTRY_DSN=${{ secrets.SENTRY_DSN }}" >> .env
+npm run build:linux
+npm run package:linux
+pwd
